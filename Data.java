fc10236e6631959e7a0df32cfeef516c2a86df29
saaikumaran2006-ai <saaikumaran2006@gmail.com>
2026-01-20 22:01:34 +0530

    Add files via upload

---
+++ Data.java
@@ -0,0 +1,28 @@
+class Data {
+public static void main(String[] args) {
+
+int age = 20;
+System.out.println("Integer value: " + age);
+        
+short sht = 1234;
+System.out.println("Short value: " + sht);      
+   
+long lng = 1234567898L;
+System.out.println("Long value: " + lng);
+
+double dbl = 99.99;
+System.out.println("Double value: " + dbl);
+
+float per = 85.5f;
+System.out.println("Float value: " + per);
+
+char grade = 'A';
+System.out.println("Character value: " + grade);
+
+boolean isJavaFun = true;
+System.out.println("Boolean value: " + isJavaFun);
+
+String name = "Saai Kumaran";
+System.out.println("String value: " + name);
+}
+}
