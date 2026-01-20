38ee5203c3173cc0a65fbe8296cde483ddeb778a
saaikumaran2006-ai <saaikumaran2006@gmail.com>
2026-01-20 22:34:36 +0530

    Add files via upload

---
+++ Dataoperation.java
@@ -0,0 +1,34 @@
+class Dataoperation {
+public static void main(String[] args) {
+
+int a=10;
+int b=3;
+
+System.out.println(a+b);
+System.out.println(a-b);
+System.out.println(a*b);
+System.out.println(a/b);
+System.out.println(a%b);
+
+long l1=100000L;
+long l2=50000L;
+System.out.println(l1+l2);
+System.out.println(l1-l2);
+System.out.println(l1*l2);
+System.out.println(l1/l2);
+
+float f1=10.5f;
+float f2=2.5f;
+System.out.println(f1+f2);
+System.out.println(f1-f2);
+System.out.println(f1*f2);
+System.out.println(f1/f2);
+
+double d1=20.75;
+double d2=4.25;
+System.out.println(d1+d2);
+System.out.println(d1-d2);
+System.out.println(d1*d2);
+System.out.println(d1/d2);
+}
+}
