0a615dfff9b742ff14f79b2aa685ed7c5b37aa6c
saaikumaran2006-ai <saaikumaran2006@gmail.com>
2026-02-03 18:57:13 +0530

    Add files via upload

---
+++ Charcase.java
@@ -0,0 +1,14 @@
+class Charcase{
+public static void main(String[] args){
+char ch='A';
+if (ch>='A' & ch<='Z'){
+System.out.println("The character is an upper case");
+}
+else if (ch>='a' & ch<='z'){
+System.out.print("The character is a lowercase");
+}
+else{
+System.out.println("It is not a chcaracter");
+}
+}
+}
