e68199dedacf9164d82c4e535b0df05815586985
saaikumaran2006-ai <saaikumaran2006@gmail.com>
2026-04-12 16:26:00 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,18 @@
+class Calculator {
+int add(int a, int b) {
+return a + b;
+}
+int add(int a, int b, int c) {
+return a + b + c;
+}
+double add(double a, double b) {
+return a + b;
+}
+
+public static void main(String[] args) {
+Calculator obj = new Calculator();
+System.out.println("Sum of 2 numbers: " + obj.add(10, 20));
+System.out.println("Sum of 3 numbers: " + obj.add(10, 20, 30));
+System.out.println("Sum of doubles: " + obj.add(5.5, 4.5));
+}
+}
