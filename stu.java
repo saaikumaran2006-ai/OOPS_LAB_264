22a2aab9849e63523f57d5881d35292b21440c29
saaikumaran2006-ai <saaikumaran2006@gmail.com>
2026-04-12 18:15:46 +0530

    Add files via upload

---
+++ stu.java
@@ -0,0 +1,25 @@
+class Student {
+private String name;
+private int age;
+public void setName(String name) {
+this.name = name;
+}
+public void setAge(int age) {
+this.age = age;
+}
+public String getName() {
+return name;
+}
+public int getAge() {
+return age;
+}
+}
+public class stu {
+public static void main(String[] args) {
+Student s = new Student();
+s.setName("Saai Kumaran");
+s.setAge(20);
+System.out.println("Name: " + s.getName());
+System.out.println("Age: " + s.getAge());
+}
+}
